ba28a76e60ea0bc329eab4cf6fccc85c7fcab1cb
devnaine-code <dev.nine0@gmail.com>
2024-10-13 19:33:40 +0900

    🔥 first commit

---
+++ src/test/java/com/devnine/todolistapp/TodoListAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devnine.todolistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
